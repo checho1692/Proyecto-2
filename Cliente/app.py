d1ad72f3fb4ac784f2ceb84d8da24545fd5c9174
checho1692 <sergioardila196@gmail.com>
2024-11-21 14:42:12 -0500

    afas
    asffs

--- Cliente/app.py
+++ Cliente/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, json
 import datetime
 
 import requests
@@ -6,8 +6,8 @@ import requests
 
 app = Flask(__name__)
 # Datos de ejemplo de usuarios
-users = {'empleado@atlas.com': 'password123', 'admin': '12345'}
-
+users = {'empleado@atlas.com': 'password123', 'admin@': '12345'}
+servidor='/enviar'
 
 @app.route('/')
 def index():
@@ -23,13 +23,31 @@ def login():
         # codigo = '12345'  # Código de ejemplo
         codigo = username
         fecha= datetime.datetime.now()
-        servidor='/enviar'
+        
         if username=='empleado@atlas.com':
             name='John Doe'
         else:
-            name=codigo
-        # return render_template('codigo.html', codigo=codigo)
-        return render_template('codigo.html', codigo=codigo,fecha=fecha,servidor=servidor,name=name)
+            name=username
+            codigo='admin@admin.com'
+        
+        url = 'http://127.0.0.1:5001/consulta'  # Reemplaza con la URL de tu servicio RESTful
+        datos = {'nombre': name}  # Los datos que quieres enviar
+        # return datos 
+        response = requests.post(url, json=datos)
+        
+        if response.status_code == 200:
+            fecha=datetime.datetime.now()
+            # repuesta=response.status_code
+            repuesta = response.json()
+            # response.append(repuesta)
+            print('Datos enviados correctamente')
+            return '<script>alert("Datos enviados correctamente");</script>' + render_template('codigo.html', codigo=codigo,fecha=fecha,servidor=servidor,name=name,empleados=repuesta)
+            # return jsonify(repuesta)
+        else:
+            print('Error al enviar los datos')
+            return '<script>alert("Datos enviados incorrectamente");</script>'    
+            # return render_template('codigo.html', codigo=codigo)
+        # return render_template('codigo.html', codigo=codigo,fecha=fecha,servidor=servidor,name=name)
     else:
         return '<script>alert("Credenciales incorrectas");</script>'+ render_template('login.html')
 
@@ -39,16 +57,40 @@ def enviar_datos():
     nombre = request.form['nombre']
     correo = request.form['correo']
     fecha = request.form['fecha']
-    url = 'http://127.0.0.1:5001/personas'  # Reemplaza con la URL de tu servicio RESTful
-    datos = {'nombre': nombre, 'correo': correo, 'fecha':  fecha}  # Los datos que quieres enviar
-    
+    url = 'http://127.0.0.1:5001/personasin'  # Reemplaza con la URL de tu servicio RESTful
+    datos = {'nombre': nombre, 'correo': correo, 'fecha': fecha}  # Los datos que quieres enviar
+    # return datos 
     response = requests.post(url, json=datos)
     
     if response.status_code == 200:
+        fecha=datetime.datetime.now()
         print('Datos enviados correctamente')
+        url = 'http://127.0.0.1:5001/consulta'  # Reemplaza con la URL de tu servicio RESTful
+        datos = {'nombre': nombre}
+        response = requests.post(url, json=datos)
+        repuesta = response.json()
+        return '<script>alert("Datos enviados correctamente");</script>' + render_template('codigo.html', codigo=correo,fecha=fecha,servidor=servidor,name=nombre,empleados=repuesta)
     else:
         print('Error al enviar los datos')
+        return '<script>alert("Datos enviados incorrectamente");</script>'
+    
 
+@app.route('/enviar1', methods=['POST'])
+def enviar_datos2():
+    nombre2 = request.form['nombre']
+    # correo = 'b'
+    # fecha = 'c'
+    # url = 'http://127.0.0.1:5001/personasin'  # Reemplaza con la URL de tu servicio RESTful
+    # datos = {'nombre': nombre, 'correo': correo, 'fecha':  fecha}  # Los datos que quieres enviar
+    return nombre2 
+    # response = requests.post(url, json=datos)
+    
+    # if response.status_code == 200:
+    #     print('Datos enviados correctamente')
+    #     return '<script>alert("Datos enviados correctamente");</script>'
+    # else:
+    #     print('Error al enviar los datos')
+    #     return '<script>alert("Datos enviados incorrectamente");</script>'
 
 if __name__ == '__main__':
     app.run(debug=True)       
